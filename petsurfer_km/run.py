2e053e4f33d2f4eb52fe23b64808c9dcb6f108b0
Paul Wighton <paul.wighton@gmail.com>
2026-02-23 12:03:41 -0500

    [FIX] short-term sqlite3 workaround for fspython (linux only)

--- petsurfer_km/run.py
+++ petsurfer_km/run.py
@@ -2,6 +2,19 @@
 
 from __future__ import annotations
 
+# Use pysqlite3 as a drop-in replacement for sqlite3 if available.
+# This is needed for FreeSurfer's fspython, which lacks the _sqlite3 C extension.
+# pysqlite3-binary only has wheels for Linux x86-64
+#
+# Be sure to run `fspython -m pip install pysqlite3-binary` if running from
+# inside fspython
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import argparse
 import logging
 import os
